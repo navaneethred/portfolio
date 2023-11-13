f2f051ccf5b785fc8a84d3bb0df6ca1556c7316b
Navaneeth Krishna K <navaneeth.red@gmail.com>
2023-11-13 08:28:46 +0530

    About Section started

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,30 @@
+import {skills} from '@/constants';
+import Image from 'next/image';
+
 export default function About() {
     return (
-      <div>About</div>
+      <section className="max-container">
+        <h1 className="head-text">
+          Hello, I'm <span className="blue-gradient_text font-semibold drop-shadow">Navaneeth</span>
+        </h1>
+
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>Software Engineer based in India, specializing in Cloud Architecture and Full-Stack Development.</p>
+        </div>
+        <div className="py-10 flex flex-col">
+          <h3 className="subhead-text">My Skills</h3>
+          <div className="mt-16 flex flex-wrap gap-12">
+            {skills.map((skill) => (
+              <div className='block-container w-20 h-20'>
+                <div className='btn-back rounded-xl' />
+                <div className='btn-front rounded-xl flex justify-center items-center'>
+                  <Image src={skill.imageUrl} alt={skill.name} width={50} height={50} className='w-1/2 h-1/2 object-contain' />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     );
   }
   
---
+++ src/constants/index.js
@@ -0,0 +1,200 @@
+export const skills = [
+    {
+        imageUrl: '/icons/css.svg',
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/express.svg',
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: '/icons/git.svg',
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: '/icons/github.svg',
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: '/icons/html.svg',
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/javascript.svg',
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/mongodb.svg',
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: '/icons/motion.svg',
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: '/icons/mui.svg',
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/nextjs.svg',
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/nodejs.svg',
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: '/icons/react.svg',
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/redux.svg',
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: '/icons/sass.svg',
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/tailwindcss.svg',
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: '/icons/typescript.svg',
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "React.js Developer",
+        company_name: "Starbucks",
+        icon: '/images/starbucks.png',
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Tesla",
+        icon: '/images/tesla.png',
+        iconBg: "#fbc3bc",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: '/images/shopify.png',
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: '/images/meta.png',
+        iconBg: "#a2d2ff",
+        date: "Jan 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: '/icons/contact.svg',
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: '/icons/github.svg',
+        link: 'https://github.com/WebFork',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: '/icons/linkedin.svg',
+        link: 'https://www.linkedin.com/in/navaneethkrishnak/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: '/icons/pricewise.svg',
+        theme: 'btn-back-red',
+        name: 'Amazon Price Tracker',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/adrianhajdin/pricewise',
+    },
+    {
+        iconUrl: '/icons/threads.svg',
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/adrianhajdin/threads',
+    },
+    {
+        iconUrl: '/icons/car.svg',
+        theme: 'btn-back-blue',
+        name: 'Car Finding App',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    {
+        iconUrl: '/icons/snapgram.svg',
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/adrianhajdin/social_media_app',
+    },
+    {
+        iconUrl: '/icons/estate.svg',
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/adrianhajdin/projects_realestate',
+    },
+    {
+        iconUrl: '/icons/summiz.svg',
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
